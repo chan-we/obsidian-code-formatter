031ec8ec96e5cfce5cde4d5f68848cc925a4dc49
chenliangwei <chenliangwei@bilibili.com>
2025-01-17 20:28:20 +0800

    feat: format ts code

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 import { Plugin } from "obsidian";
+import { format } from "prettier";
+import * as babelPlugin from "prettier/plugins/babel";
+import * as typescriptPlugin from "prettier/plugins/typescript";
+import * as prettierPluginEstree from "prettier/plugins/estree";
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -6,6 +10,16 @@ interface MyPluginSettings {
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: "default",
+};
+
+const plugins = [babelPlugin, typescriptPlugin, prettierPluginEstree];
+
+const formatCode = (code: string) => {
+	return format(code, {
+		semi: false,
+		parser: "typescript",
+		plugins,
+	});
 };
 
 export default class MyPlugin extends Plugin {
@@ -18,8 +32,39 @@ export default class MyPlugin extends Plugin {
 			id: "format-code",
 			name: "Format Code",
 			editorCallback(editor, ctx) {
-				const content = editor.getDoc().getValue();
-				console.log(content);
+				const doc = editor.getDoc();
+
+				let flag = false;
+				let temp = "";
+				let start = Number.NaN;
+				for (let i = 0; i < doc.lineCount(); i++) {
+					const line = doc.getLine(i);
+					if (line.trim().startsWith("```")) {
+						flag = !flag;
+						if (flag) {
+							start = i;
+							continue;
+						}
+
+						if (!Number.isNaN(start)) {
+							formatCode(temp).then((res) => {
+								doc.replaceRange(
+									res,
+									{ line: start + 1, ch: 0 },
+									{ line: i, ch: 0 },
+								);
+								temp = "";
+								start = Number.NaN;
+							});
+						}
+
+						continue;
+					}
+
+					if (flag) {
+						temp += line + "\n";
+					}
+				}
 			},
 		});
 	}
